5bbf726527c0ec84cfcd41447f4485e138ad31d1
lampo1024 <lampo1024@gmail.com>
2017-02-06 17:42:57 +0800

    Add files via upload

---
+++ vue-tabpanel.min.js
@@ -0,0 +1,32 @@
+import TabsView from './Tabs.vue'
+export default function install(Vue) {
+    if (install.installed) {
+        return
+    }
+    install.installed = true
+
+    Object.defineProperty(Vue.prototype, '$taber', {
+        get() {
+            return this.$root._taber
+        }
+    })
+
+    Object.defineProperty(Vue.prototype, '$tab', {
+        get() {
+            return this.$options.$tab
+        }
+    })
+
+    TabsView.methods.getVue = () => Vue
+    Vue.component('VueTabpanel', TabsView)
+
+    Vue.mixin({
+        beforeCreate() {
+            if (this.$options.taber) {
+                this._taber = this.$options.taber
+            } else if (this.$options.__taber) {
+                this._taber = this.$options.__taber
+            }
+        }
+    })
+}
