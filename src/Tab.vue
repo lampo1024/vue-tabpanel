bc74268d4037dd07641eeded9bc6148a2f638ee0
Liubo <jumi.ssp@outlook.com>
2017-02-06 18:02:51 +0800

    add allow colsable

--- src/Tab.vue
+++ src/Tab.vue
@@ -1,5 +1,5 @@
 <template>
-<li :class="{'active': tabData.active, 'loading': tabData.loading}">{{tabData.params.title}}<span class="btn-close" @click.stop="close">&times;</span></li>
+<li :class="{'active': tabData.active, 'loading': tabData.loading}">{{tabData.params.title}}<span v-if="tabData.allowClose" class="btn-close" @click.stop="close">&times;</span></li>
 </template>
 <script>
     export default {
